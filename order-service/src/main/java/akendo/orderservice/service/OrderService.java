2b5988229b19b1cbd78d9c7f5e448d480d11fc04
Ruslan Kadirov <ruslankadirov1337@gmail.com>
2026-06-07 03:36:19 +0500

    feat(order): add some buisness logic

---
+++ order-service/src/main/java/akendo/orderservice/service/OrderService.java
@@ -0,0 +1,50 @@
+package akendo.orderservice.service;
+
+import  akendo.orderservice.domain.Order;
+import akendo.orderservice.exceptions.OrderNotFoundException;
+import akendo.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    @Transactional
+    public Order createOrder(UUID customerId, BigDecimal totalAmount) {
+        Order order = Order.create(customerId, totalAmount);
+        return orderRepository.save(order);
+    }
+
+    @Transactional(readOnly = true)
+    public Order getOrder(UUID orderId) {
+        return orderRepository.findById(orderId)
+                .orElseThrow(() -> new OrderNotFoundException(orderId));
+    }
+
+    @Transactional(readOnly = true)
+    public List<Order> getOrders() {
+        return orderRepository.findAll();
+    }
+
+    @Transactional
+    public Order payOrder(UUID orderId) {
+        Order order = getOrder(orderId);
+        order.pay();
+        return orderRepository.save(order);
+    }
+
+    @Transactional
+    public Order cancelOrder(UUID orderId) {
+        Order order = getOrder(orderId);
+        order.cancel();
+        return orderRepository.save(order);
+    }
+}
