bbbc3c0f573d91e0f01dd01e036eda6ca83ec7ff
Ruslan Kadirov <ruslankadirov1337@gmail.com>
2026-06-08 15:00:19 +0500

    feat(order): add new send methods

--- order-service/src/main/java/akendo/orderservice/messaging/producer/OrderProducer.java
+++ order-service/src/main/java/akendo/orderservice/messaging/producer/OrderProducer.java
@@ -1,7 +1,9 @@
 package akendo.orderservice.messaging.producer;
 
 import akendo.orderservice.config.KafkaTopicConfig;
+import akendo.orderservice.messaging.events.OrderCancelledEvent;
 import akendo.orderservice.messaging.events.OrderCreatedEvent;
+import akendo.orderservice.messaging.events.OrderPaidEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -9,11 +11,27 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class OrderProducer {
-    private final KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void sendOrderCreated(OrderCreatedEvent event) {
         kafkaTemplate.send(
                 KafkaTopicConfig.ORDER_CREATED_TOPIC,
+                event.orderId().toString(),
+                event
+        );
+    }
+
+    public void sendOrderPaid(OrderPaidEvent event) {
+        kafkaTemplate.send(
+                KafkaTopicConfig.ORDER_PAID_TOPIC,
+                event.orderId().toString(),
+                event
+        );
+    }
+
+    public void sendOrderCancelled(OrderCancelledEvent event) {
+        kafkaTemplate.send(
+                KafkaTopicConfig.ORDER_CANCELED_TOPIC,
                 event.orderId().toString(),
                 event
         );
