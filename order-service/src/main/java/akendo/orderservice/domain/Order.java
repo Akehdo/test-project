25d58c02dd75e295abb75554f360e40b91660924
Ruslan Kadirov <ruslankadirov1337@gmail.com>
2026-06-11 17:45:43 +0500

    update model: add versiod and manual created_at

--- order-service/src/main/java/akendo/orderservice/domain/Order.java
+++ order-service/src/main/java/akendo/orderservice/domain/Order.java
@@ -1,17 +1,10 @@
 package akendo.orderservice.domain;
 
 import akendo.orderservice.exceptions.BadRequestException;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
@@ -25,28 +18,33 @@ import java.util.UUID;
 public class Order {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(nullable = false)
+    @Column(name = "customer_id",nullable = false)
     private UUID customerId;
 
-    @Column(nullable = false, precision = 19, scale = 2)
+    @Column(name = "total_amount",nullable = false, precision = 19, scale = 2)
     private BigDecimal totalAmount;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 30)
     private OrderStatus status;
 
+    @Version
+    private Long version;
+
+    @Column(name = "paid_at")
     private Instant paidAt;
 
+    @Column(name = "cancelled_at")
     private Instant cancelledAt;
 
-    @CreationTimestamp
-    @Column(nullable = false, updatable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt;
 
     @UpdateTimestamp
-    @Column(nullable = false)
+    @Column(name = "updated_at", nullable = false)
     private Instant updatedAt;
 
     public static Order create(UUID customerId, BigDecimal totalAmount) {
@@ -58,10 +56,10 @@ public class Order {
         }
 
         Order order = new Order();
-        order.id = UUID.randomUUID();
         order.customerId = customerId;
         order.totalAmount = totalAmount;
         order.status = OrderStatus.CREATED;
+        order.createdAt = Instant.now();
 
         return order;
     }
